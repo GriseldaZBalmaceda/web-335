cda4d629db0e166b1bd6b004f5daa084df4e3247
GriseldaZBalmaceda <gzbalmaceda@my365.bellevue.edu>
2019-06-22 12:52:59 -0500

    committing calculator and addded code attribution and comments

---
+++ Week 8/Balmaceda_calculator.py
@@ -0,0 +1,21 @@
+#============================================
+# Title:  calculator
+# Author: Griselda Balmaceda 
+# Date:   22 June 2019
+# Description: Creating simple add, subtract and divide functions
+#===========================================
+
+#creating the add function
+def add(a,b):
+    return a + b 
+
+#creating substract function
+def substract(a,b):
+    return a-b
+#creating divide function
+def divide(a,b):
+    return a/b
+#printing output of all three function calls
+print(add(1,2))
+print(substract(4,1))
+print(divide(8,2))
--- Week 8/Balmaceda_hello_world.py
+++ Week 8/Balmaceda_hello_world.py
@@ -1,1 +1,7 @@
+#============================================
+# Title:  Hello World
+# Author: Griselda Balmaceda 
+# Date:   22 June 2019
+# Description: Hello World!
+#===========================================
 print('Hello World')
